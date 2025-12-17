506d54db9796569f5204fd7de09fe62a86a9316e
BaeSeBin <143635021+SebinBae@users.noreply.github.com>
2025-12-17 13:29:07 +0900

    first commit!

---
+++ src/main/java/com/sebin/secondhand_market/SecondhandMarketApplication.java
@@ -0,0 +1,13 @@
+package com.sebin.secondhand_market;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecondhandMarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecondhandMarketApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sebin/secondhand_market/SecondhandMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sebin.secondhand_market;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecondhandMarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
